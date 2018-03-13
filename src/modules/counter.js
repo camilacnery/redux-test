cc755b10e7ece531adc7f5f6199e7cdd176a3755
Camila Nery <camilacnery@gmail.com>
2018-03-12 23:08:43 -0300

    Created redux example

---
+++ src/modules/counter.js
@@ -0,0 +1,37 @@
+import { RSAA } from 'redux-api-middleware';
+
+// Action Types
+export const FETCH_TOGGLES_PENDING = 'toggles/togglesPending';
+export const FETCH_TOGGLES_SUCCESS = 'toggles/togglesSuccess';
+export const FETCH_TOGGLES_ERROR = 'toggles/togglesError';
+
+// Action Creators
+export const fetchToggles = () => ({
+  [RSAA]: {
+    endpoint: 'http://localhost:3000/toggles',
+    method: 'GET',
+    types: [FETCH_TOGGLES_PENDING, FETCH_TOGGLES_SUCCESS, FETCH_TOGGLES_ERROR],
+  },
+});
+
+// Reducer
+const reducer = (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_TOGGLES_PENDING:
+      return { ...state, pending: true };
+    case FETCH_TOGGLES_SUCCESS:
+      return { ...state, ...action.payload, pending: false };
+    case FETCH_TOGGLES_ERROR:
+      return { pending: false };
+    default:
+      return { ...state };
+  }
+};
+// Exports
+export default reducer;
+
+const togglesActions = {
+  fetchToggles,
+};
+
+export { togglesActions };
---
+++ src/modules/counter.test.js
@@ -0,0 +1,42 @@
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { apiMiddleware } from 'redux-api-middleware';
+import nock from 'nock';
+import reducer, { togglesActions, FETCH_TOGGLES_PENDING, FETCH_TOGGLES_SUCCESS } from './counter';
+
+require('isomorphic-fetch');
+
+const mockStore = configureMockStore([apiMiddleware, thunk]);
+
+describe('Toggles actions', () => {
+  afterEach(() => {
+    nock.cleanAll();
+  });
+
+  it('should dispatch "TOGGLES_SUCCESS" when fetchToggles is done', async () => {
+    nock('http://localhost:3000')
+      .get('/toggles')
+      .reply(200, { toggle1: true, toggle2: false });
+
+    const store = mockStore({});
+    const expectedActions = [
+      { type: FETCH_TOGGLES_PENDING },
+      { type: FETCH_TOGGLES_SUCCESS, payload: { toggle1: true, toggle2: false } },
+    ];
+
+    await store.dispatch(togglesActions.fetchToggles());
+
+		expect(store.getActions()).toMatchObject(expectedActions);
+  });
+});
+
+describe('Toggles reducer', () => {
+  it('should append received company', () => {
+    const result = reducer({}, {
+      payload: { toggle1: true },
+      type: FETCH_TOGGLES_SUCCESS,
+    });
+
+    expect(result.toggle1).toEqual(true);
+  });
+});
